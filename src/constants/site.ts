95e6987e3c9c648f3d9b96dd45178b910de81f7b
himanshudhakan <himanshudhakan9@gmail.com>
2026-08-22 14:03:38 +0530

    fix: update contact email to info@ragainternational.com for clarity

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
   defaultOgImage:
     'https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?auto=format&fit=crop&w=1600&q=80',
   phone: '+91 94097 22614',
-  email: 'hello@ragainternational.com',
+  email: 'info@ragainternational.com',
   whatsappUrl: 'https://wa.me/919409722614',
   instagramUrl: 'https://www.instagram.com/raga_international_/',
   instagramHandle: '@raga_international_',
